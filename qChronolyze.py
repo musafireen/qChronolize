621a50431011a83b2e58bc7423416c04325268fb
musafireen <kalabhairv@gmail.com>
2024-04-28 09:15:17 +0600

    1st commit

---
+++ qChronolyze.py
@@ -0,0 +1,366 @@
+def confPres(pres):
+    '''Sets configuration'''
+    presDict={'1':'table','2':'plot'}
+    if pres not in presDict.values():
+      if pres in presDict.keys():
+        pres=presDict[pres]
+      else:
+        if pres=='' or pres=='None':
+            print('No presentation style provided')
+        else:
+            print('Invalid presentation style:', pres)
+        pres=''
+        import re
+        with open('./cnf.txt') as f:
+            txt = f.read()
+        for m in re.compile('(?<=presentation\=).*?(?=$|\n)').findall(txt):
+            pres+=m
+        if len(pres) > 1:
+            print(pres, 'found in cnf.txt')
+        while pres not in presDict.values():
+            print(f'{pres} not valid presentation style')
+            pres = str(input(
+               'Please provide presentation style:\n1 for table \n2 for plot (default)'+ 
+               '\n\n(You can change later from \'cnf.txt\')'
+               )).lower() or 'plot'
+            if pres == '' or pres == 'None':
+               pres = '2'
+            if pres in presDict.keys() or pres in presDict.values():
+              if pres in presDict.keys():
+                pres = presDict[pres]
+              import re
+              import functools
+              newTxt = re.compile(r'(?=^|\n)presentation\=.*(?=\n|$)').sub('',txt) + f'presentation={pres}\n'
+              with open('./cnf.txt','w') as f:
+                    f.write(newTxt)
+    print(f'{pres} style choosen for presentation.')
+    return pres
+
+def confLng(refLng):
+    lngDict={'1':'arabic','2':'bengali','3':'english'}
+    if refLng not in lngDict.values():
+      if refLng in lngDict.keys():
+          refLng=lngDict[refLng]
+      else:
+        if refLng=='' or refLng=='None':
+            print('No tafsir language provided')
+        else:
+            print('Invalid tafsir language:', refLng)
+        refLng=''
+        import re
+        with open('./cnf.txt') as f:
+            txt = f.read()
+        for m in re.compile('(?<=refLng\=).*(?=$|\n)').findall(txt):
+            refLng+=m
+        if len(refLng) > 0:
+            print(refLng, 'found in cnf.txt')
+        while refLng not in lngDict.values():
+            print('no valid tafsir language!')
+            refLng = str(input(
+               'Please choose tafsir language:\n1 for Arabic \n2 for Bengali \n3 for English (default) '+
+               '\n\n(You can change later from \'cnf.txt\')'
+               )).lower()
+            if refLng == '' or refLng == 'None':
+               refLng = '3'
+            if refLng in lngDict.values() or refLng in lngDict.keys():
+              if refLng in lngDict.keys():
+                refLng=lngDict[refLng]
+              import re
+              newTxt = re.compile(r'(?=^|\n)refLng\=.*(?=\n|$)').sub('',txt) + f'refLng={refLng}\n'
+              with open('./cnf.txt','w') as f:
+                  f.write(newTxt)
+    print(f'{refLng} language choosen for reference.')
+
+    tafsDict={"arabic":"ar-tafsir-al-tabari","bengali":"bn-tafseer-ibn-e-kaseer","english":"en-tafisr-ibn-kathir"}
+    return tafsDict[refLng]
+
+
+
+def dataGrabber(tafs,rt,flt=''):
+    '''Grabs data from corpus.quran.com & formats them'''
+
+    flt=str(flt).lower()
+
+    import requests
+    import re
+    import html2text
+    import json
+
+    links = [
+        "https://corpus.quran.com/qurandictionary.jsp?q=",
+        "https://corpus.quran.com/search.jsp?q="
+        ]
+
+    data=[]
+
+
+
+    propDat = False
+    import os
+    if os.path.isfile(f'data/{rt}'):
+      try:
+        with open(f'data/{rt}') as f:
+          tmpDat = json.loads(f.read())
+        if type(tmpDat) == type([]):
+          data = tmpDat
+          propDat = True
+      except:
+        propDat = False
+    if not propDat:
+      for lnk in links:
+          txt = requests.get(f"{lnk}{rt}").text
+
+          txt_ex_list = re.findall('<table class="taf".*?</table>', txt)
+
+          txt_ex=''
+          for i in txt_ex_list:
+              txt_ex += i
+
+          txt_ex=re.sub(
+              '<\/*span[^>]*>',
+              '',
+              txt_ex
+          )
+          txt_ex
+
+          str2 = html2text.html2text(txt_ex)
+
+          strng_prepare = re.sub(
+              "###[^\n]*\n",
+              "",
+              str2
+              )
+
+          strng_prepare = strng_prepare.replace(',','').replace('"', '`').replace('\'','`')
+
+          strng_sub =re.sub(
+              r'\((\d*):(\d*):(\d*)\)\s*_([^\d+\t]+?)_\|\s*\[([^\d\t]+?)\]\(.*\)\|([^\()]+)\n',
+              '{ "surah:ayah": "\\1:\\2", "position": \\3, "word": "\\4", "meaning": "\\5", "ayah_link": "<a style=\'color:black\' href=\'https://quran.com/\\1:\\2/tafsirs/'+tafs+'\'>\\6</a>" }, \n',
+              strng_prepare
+          )
+
+          strng_sub = '[' + strng_sub.replace('\n',' ')[:-3] + ']'
+
+          dat = json.loads(strng_sub)
+
+
+          if type(dat) != type([]):
+            dat = []
+          elif len(dat) == 0:
+            dat = []
+          elif type(dat[0]) != type({"a":"b"}):
+            dat = []
+          elif 'word' not in dat[0].keys():
+            dat = []
+          else:
+            misMatch = False
+            if 'adam' in dat[0]['meaning'].lower():
+              if rt.lower() != 'adam' and rt != 'A^Edam':
+                misMatch = True
+            if misMatch:
+              dat = []
+          data += dat
+
+    with open(f'data/{rt}', 'w') as f:
+      f.write(json.dumps(data))
+
+
+
+    import pandas as pd
+
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_colwidth', None)
+
+    df = pd.DataFrame.from_records(data)
+    df = df.drop_duplicates(['surah:ayah','position'])
+    df = df.drop(['position'], axis=1)
+    df = df[df['meaning'].str.contains(flt, case=False)]
+    df["query"] = f'{rt} ({flt})'
+
+    return df
+
+
+
+def sortchron(dicti={},refLng='',pres=''):
+    ''' Sorts ayahs chronologically '''
+
+    pres = confPres(pres=pres)
+    tafs = confLng(refLng=refLng)
+
+
+    """ to tweak the sorter tweak the "surOrd.tsv" file manually """
+
+
+    import json
+    import csv
+    with open('qdict.json') as f:
+      qStr = f.read()
+    qDict = json.loads(qStr)
+
+    with open('surOrd.tsv') as f:
+      surOrd = [row for row in csv.DictReader(f, delimiter='\t') ]
+
+    sorter = [''] * 6236
+    bef = {}
+    aft = {}
+    befaft = []
+
+    for v in surOrd:
+      s=v["surah"]
+      if "exception" in v.keys():
+        exs = v["exception"]
+        if type(exs) == type(''):
+          exs = json.loads(exs)
+        if len(exs) > 0:
+          for ex in exs:
+            ar = ex["ayah_range"].split('-')
+            al = ar if len(ar) == 1 else list(range(int(ar[0]),int(ar[1])+1)) if len(ar) == 2 else []
+            if "before" in ex.keys():
+              if ex["before"] != '':
+                if ex["before"] not in bef.keys():
+                  bef[ex["before"]]=[]
+                for ay in al:
+                  bef[ex["before"]].append(f'{s}:{ay}')
+                  befaft.append(f'{s}:{ay}')
+            if "after" in ex.keys():
+              if ex["after"] != '':
+                if ex["after"] not in aft.keys():
+                  aft[ex["after"]]=[]
+                for ay in al:
+                  aft[ex["after"]].append(f'{s}:{ay}')
+                  befaft.append(f'{s}:{ay}')
+
+    i = 0
+
+    for v in surOrd:
+      s=v["surah"]
+      for a in range(1, qDict[s]["verse_count"]+1):
+        s_a= f'{s}:{a}'
+        if s_a in bef.keys():
+          for sa in bef[s_a]:
+            sorter[i] = sa
+            i += 1
+        if s_a not in befaft:
+          sorter[i] = s_a
+          i += 1
+        if s_a in aft.keys():
+          for sa in aft[s_a]:
+            sorter[i] = sa
+            i += 1
+    
+    print(sorter[-1], len(sorter))
+
+    if type(dicti) != type({}):
+        print("Invalid root-filter key value pairs")
+        dicti={}
+    if dicti=={}:
+        finished=False
+        while finished==False:
+            inpLs=str(input(
+                """
+                Enter comma-separated list of
+                '::' double colon-separated pairs of
+                word/root and meaning-filter regex like:
+
+                \'$Tn::devil,rwH::(?:spirit|soul)\'
+
+                or \'sHr,jnn::jinn\n\'
+                """
+                )
+            ).split(',')
+            if type(inpLs) == type([]) and len(inpLs) >= 1:
+              for obj in inpLs:
+                pair = obj.strip().split('::')
+                if len(pair) == 2:
+                  dicti[pair[0]]=pair[1]
+                  finished=True
+                if len(pair) == 1:
+                  dicti[pair[0]]=''
+                  finished=True
+
+    import pandas as pd
+    df = pd.DataFrame()
+    for rt in dicti.keys():
+      df = pd.concat([df,dataGrabber(tafs,rt,dicti[rt])])
+      df.reset_index(drop=True,inplace=True)
+      df.reset_index(inplace=True)
+
+    if pres=='table':
+      df.drop(columns=["query"],inplace=True)
+      compareDict = {}
+      for i in range(len(sorter)):
+          compareDict[sorter[i]]=i+1
+
+      df.insert( 0, 'verses_before',    list(
+              map(
+                lambda x: compareDict[x],
+                # df["surah"]
+                df["surah:ayah"]
+              )
+          )
+      )
+
+      df['surah:ayah'] = pd.Categorical(df['surah:ayah'], categories=sorter, ordered=True)
+
+      from IPython.core.display import HTML
+
+      return HTML(df.sort_values( ## 'sort' changed to 'sort_values'
+            # ["surah","ayah"]
+            ['surah:ayah']
+        ).to_html(render_links=True,escape=False,index=False))
+
+
+    if pres=='plot':
+      import plotly.express as px
+      # import plotly.graph_objects as go
+      # from plotly.subplots import make_subplots
+      # from plotly.offline import iplot, init_notebook_mode
+
+      # init_notebook_mode()
+      df["ayah_link"] = df["surah:ayah"] + df["ayah_link"]
+      fig = px.scatter(
+         df,
+         x='surah:ayah',
+         y='ayah_link',
+         hover_data={'ayah_link':True,'query': False, 'surah:ayah': False},
+         color='query',
+         color_continuous_scale=["green","yellow","orange","red"],
+         height=(len(df))*8,
+         width=(len(sorter))/5,
+         labels={
+          "surah:ayah": " (earlier)   -->  'Surah:Ayah' (Chronologically Ordered)  -->   (latter)",
+          "ayah_link": ""
+         }
+      )
+
+      # fig = make_subplots(
+      #     specs=[[{"secondary_y": True}]]
+      # )
+
+      # # fig.layout=go.Layout(clickmode='event+select')
+
+      # for rt in dicti:
+      #     if rt != '' and rt != None:
+      #       tbl = dataGrabber(tafs,rt,dicti[rt])
+      #       fig.add_trace(
+      #           go.Scatter(
+      #               y=tbl.ayah_link,
+      #               x=tbl['surah:ayah'],
+      #               name=rt +  f' ({dicti[rt]})' if dicti[rt] != None or dicti[rt] != '' else '',
+      #               mode='markers',
+      #               # customdata=dataGrabber(tafs,rt,dicti[rt]).ayah_link,
+      #               hoverinfo='y+x'
+      #               # marker=dict(
+      #               # color='rgb(34,163,192)'
+      #               # )
+      #           ),
+      #           secondary_y=True
+      #       )
+      fig.update_xaxes(categoryorder='array',categoryarray=sorter,range=[0,len(sorter)])
+      fig.update_yaxes(showticklabels=False,range=[0,len(df)])
+      fig.show()
+      # iplot(fig)
+      # import matplotlib.pyplot as plt
+      # plt.show()
+
+  
---
+++ readme.md
@@ -0,0 +1,29 @@
+# qChronolyze
+Try on google <a href="https://colab.research.google.com/drive/1tK21rWFKUIWH_3-lMX99d2SCNJx1mSnJ?usp=sharing" target="_blank">Colab<a>
+
+[![alt text](https://img.youtube.com/vi/2es9qayH5oo/0.jpg)](https://www.youtube.com/embed/2es9qayH5oo?si=3oEls05_2FFtQjEh&amp;start=80)
+
+<a href="https://ibb.co/vZjz4ZZ"><img src="https://i.ibb.co/vZjz4ZZ/vlcsnap-2024-04-23-21h02m52s708.png" alt="vlcsnap-2024-04-23-21h02m52s708" border="0"></a>  <a href="https://ibb.co/p0YPPRr"><img src="https://i.ibb.co/p0YPPRr/vlcsnap-2024-04-23-21h03m12s122.png" alt="vlcsnap-2024-04-23-21h03m12s122" border="0"></a> 
+
+<a href="https://ibb.co/5khCqsD"><img src="https://i.ibb.co/5khCqsD/vlcsnap-2024-04-23-21h03m40s994.png" alt="vlcsnap-2024-04-23-21h03m40s994" border="0"></a>  <a href="https://ibb.co/jMMVvx0"><img src="https://i.ibb.co/jMMVvx0/vlcsnap-2024-04-23-21h02m44s809.png" alt="vlcsnap-2024-04-23-21h02m44s809" border="0"></a>
+
+1) Shows at what point in the composition of Quran particular words and concepts were introduced according to traditional account
+2) Visualizes relative frequency of multiple words, or frequency of single word in different periods
+3) both tabular and scatterplot visualization
+4) you can search by either words, or word roots 
+5) Clickable data points & hyperlinks for context of each instance of a word used in the quran ([Tafsir](https://quran.com/2:102/tafsirs/en-tafisr-ibn-kathir) in 3 languages: Arabic, English & Bengali)
+
+## transliteration scheme
+if you are searching by word root, follow [this scheme](https://corpus.quran.com/java/buckwalter.jsp) for transliteration
+
+## corpus
+
+[corpus.quran.com](https://corpus.quran.com/qurandictionary.jsp?q=klm) gathers all the data about individual words, but lacks chronological data, and comparing functionality.
+
+## chronological order:
+The chronology is based on traditional [Ibn Abbas account](https://tanzil.net/docs/revelation_order)
+
+if you are a researcher who uses a different chronology of surahs/verses you can tweak the "surOrd.tsv" file:
+1) by manually moving around the lines for each surah, 
+2) changing the values in "before"/"after" and "ayah_range" in "exception" fields.
+
