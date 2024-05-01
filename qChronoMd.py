8fe221c93b2c7d3dc67d1e7c800fb5a4d18543ec
musafireen <kalabhairv@gmail.com>
2024-05-01 22:19:22 +0600

    qChronoMd

---
+++ qChronoMd.py
@@ -0,0 +1,34 @@
+from qChronolyze import filtDown, getSorter
+
+def qChronoMd(dicti,flnm):
+    mdFile = f'data/compare/{flnm}.md'
+    instLstAgg = []
+    queriesLeft = { f'{k} ({v})' for k,v in dicti.items() }
+    alreadyRefed = []
+    for rt in dicti.keys():
+        instLst = filtDown(rt,dicti[rt])
+        instLstAgg += instLst
+    # instLstAgg = [ { 
+    #     **row, 
+    #     "ayah_link": f"<a style='color:rgb(50,50,200)' href='https://quran.com/{row['surah:ayah']}/tafsirs/{tafs}'>{row['ayah_link']}</a>"
+    #     } for row in instLstAgg ]
+    import pandas as pd
+
+    df = pd.DataFrame(instLstAgg)
+    sorter = getSorter()
+    df['surah:ayah'] = pd.Categorical(df['surah:ayah'], categories=sorter, ordered=True)
+    df.sort_values(["surah:ayah","position"],inplace=True)
+    # df.reset_index(drop=True,inplace=True)
+    sortedRecs = df.to_dict(orient='records')
+
+    for rec in sortedRecs:
+        if rec["query"] in queriesLeft:
+            with open(mdFile, 'a') as f:
+                f.write(f'\n# {rec["query"]}\n')
+                queriesLeft.remove(rec["query"])
+        if rec["surah:ayah"] not in alreadyRefed:
+            with open(mdFile, 'a') as f:
+                f.write(f'\n![[QsortedRukued#{rec["surah:ayah"]}]]\n')
+            alreadyRefed.append(rec["surah:ayah"])
+        
+
