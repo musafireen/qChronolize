103f7c30e81e5bba6d2c0b7d3e41ab42e2b50e20
musafireen <kalabhairv@gmail.com>
2024-06-23 20:49:44 +0600

    qChronomd major update

--- qChronoMd.py
+++ qChronoMd.py
@@ -7,14 +7,7 @@ def qChronoMd(dicti,flnm,tafs):
     queriesLeft = { f'{list(item.keys())[0]} ({list(item.values())[0]})' for item in dicti }
     alreadyRefed = []
     instLstAgg = aggregLsts(dicti,tafs)
-    # for rt in dicti.keys():
-    #     instLst = filtDown(rt,dicti[rt])
-    #     instLst = [ { **row , "query" : f"{rt} ({dicti[rt]})"} for row in instLst ]
-    #     instLstAgg += instLst
-    # instLstAgg = [ { 
-    #     **row, 
-    #     "ayah_link": f"<a style='color:rgb(50,50,200)' href='https://quran.com/{row['surah:ayah']}/tafsirs/{tafs}'>{row['ayah_link']}</a>"
-    #     } for row in instLstAgg ]
+
     import pandas as pd
 
     df = pd.DataFrame(instLstAgg)
@@ -24,18 +17,105 @@ def qChronoMd(dicti,flnm,tafs):
     # df.reset_index(drop=True,inplace=True)
     sortedRecs = df.to_dict(orient='records')
 
+
+    repDic = {}
+
     for rec in sortedRecs:
-        if rec["surah:ayah"] not in alreadyRefed:
-            with open(mdFile, 'a') as f:
-                f.write(
-                    f'\n# Q:{rec["surah:ayah"]}\n'
-                    + f'\n[Q.{rec["surah:ayah"]}](https://quran.com/{rec["surah:ayah"]}/tafsirs/{tafs})\n'
-                    + f'\n![[Qrsed#{rec["surah:ayah"]}]]\n'
-                )
-            alreadyRefed.append(rec["surah:ayah"])
-        if rec["query"] in queriesLeft:
-            with open(mdFile, 'a') as f:
-                f.write(f'\n## {rec["query"]}\n')
-                queriesLeft.remove(rec["query"])
+        if rec["surah:ayah"] not in repDic.keys():
+            repDic[rec["surah:ayah"]] = {
+                'string': f'\n[Q.{rec["surah:ayah"]}](https://quran.com/{rec["surah:ayah"]}/tafsirs/{tafs})\n'
+                            + f'\n![[Qrsed#{rec["surah:ayah"]}]]\n',
+                'queries': [
+                    {
+                        'query': rec["query"],
+                        'postquery': ''
+                    }
+                ]
+            }
+        else:
+            repDic[rec["surah:ayah"]]["queries"].append(
+                {
+                    'query': rec["query"], 
+                    'postquery': '',
+                }
+            )
+
+
+    print('\nrepDic before checking md: ', repDic, '\n')
+
+    import re
+
+    with open(mdFile) as f:
+        strn = f.read()
+
+    rep = re.compile(
+            '\n#\s*(?:Q\:){0,1}(\d{1,}\:\d{1,})(.*?(?=\n#|$))(?:(\n##\s{1,}.*?(?=\n#\s{1,}|$))){0,1}',
+            re.DOTALL
+        ).findall(
+
+        strn
+    )
+
+
+    # print(len(rep))
+
+    for setty in rep:
+        if setty[0] not in repDic:
+            repDic[setty[0]] = { 
+                'string' : setty[1] , 
+                'queries': [
+                    {
+                        'query': quer[0],
+                        'postquery': quer[1],
+                    
+                    }
+                    # quer
+                    for quer in re.compile(
+                        '\n##\s{1,}([^\n]{1,}(?=\n))(.*?(?=\n))',    
+                        re.DOTALL,
+                    ).findall(
+                        setty[2] 
+                    )
+                ]
+                    if len(setty) > 2 else None 
+            }
+        else:
+           for quer in re.compile(
+                        '\n##\s{1,}([^\n]{1,}(?=\n))(.*?(?=\n))',    
+                        re.DOTALL,
+                    ).findall(
+                        setty[2] 
+                    ):
+                repDic[setty[0]]['queries'].append({
+                        'query': quer[0],
+                        'postquery': quer[1],
+                    
+                    })
+        
+
+    print('\nrepDic after checking md: ', repDic, '\n')
+    # print(repDic)
+
+    sorterIdxs = [sorter.index(suray) for suray in repDic.keys()]
+
+    surAyOrdered = [sorter[idx] for idx in sorterIdxs]
+    
+    newStr = ''
+
+    for surAy in surAyOrdered:       
+        newStrUp =   f'\n# Q:{surAy}\n' + f'\n[Q.{surAy}](https://quran.com/{surAy}/tafsirs/{tafs})\n' + f'\n![[Qrsed#{surAy}]]\n'
+
+        newStr += newStrUp
+
+        alreadyRefed.append(rec["surah:ayah"])
+        for query in repDic[surAy]['queries']:
+            if query['query'] in queriesLeft:
+                print(f'\nquery left at {surAy} : {query["query"]}')
+                newStrUp = f'\n## {query["query"]}\n' + query['postquery']
+                newStr += newStrUp
+                queriesLeft.remove(query["query"])
+
+    with open(mdFile, 'w') as f:
+        f.write(newStr)
         
 
